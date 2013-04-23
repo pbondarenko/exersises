89888ae55d7b80dcedc0d0ae6f2ca04938a0c902
Polina Bondarenko <pbondarenko@mirantis.com>
2013-04-23 19:05:11 +0400

    changed 2.1.c

--- 2.1.c
+++ 2.1.c
@@ -2,16 +2,15 @@
 #include <limits.h>
 #include <float.h>
 int main() {
-	printf("%d\n", sizeof(long));
-	printf("signed char %d %d\n", SCHAR_MIN, SCHAR_MAX);
-	printf("unsigned char %d\n", UCHAR_MAX);
+	printf("signed char %c %c\n", SCHAR_MIN, SCHAR_MAX);
+	printf("unsigned char %u\n", UCHAR_MAX);
 	printf("signed short int %d %d\n", SHRT_MIN, SHRT_MAX);
-	printf("unsigned short %d\n", USHRT_MAX);
+	printf("unsigned short %u\n", USHRT_MAX);
 	printf("signed int %d %d\n", INT_MIN, INT_MAX);
-	printf("unsigned int %lld\n", UINT_MAX);
+	printf("unsigned int %u\n", UINT_MAX);
 	printf("signed long int %lld %lld\n", LLONG_MIN, LLONG_MAX);
 	printf("float %f %f\n", FLT_MIN, FLT_MAX);
-	printf("double %f %f\n", DBL_MIN, DBL_MAX);
-	printf("long double %f %f\n", LDBL_MIN, LDBL_MAX);
+	printf("double %lf %lf\n", DBL_MIN, DBL_MAX);
+	printf("long double %Lf %Lf\n", LDBL_MIN, LDBL_MAX);
 	return 0;
 }
