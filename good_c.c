84d4adb7d5cb5ab9bc8745b0c0a3e921c0f5c6ef
Polina Bondarenko <pbondarenko@mirantis.com>
2013-04-25 16:32:37 +0400

    changed good_c

--- good_c.c
+++ good_c.c
@@ -1,30 +1,30 @@
 #include "my_lib.h"
 
-char * good_c (char * line) {
+good_c_answer good_c (char * line) {
 	char * line_without_comments = delete_comments(line);
 	if (!line_without_comments) {	
-		return "error in comments";
+		return ERROR_COMMENTS;
 	}
 	char * line_without_escape = delete_escape(line_without_comments);
 	free(line_without_comments);
 	if (!line_without_escape) {
-		return "error in escape-sequence";
+		return ERROR_ESCAPE;
 	}
 	char * line_without_char = delete_char(line_without_escape);
 	free(line_without_escape);
 	if (!line_without_char) {
-		return "error in char constants";
+		return ERROR_CHAR;
 	}
 	char * line_without_string = delete_string(line_without_char);
 	free(line_without_char);
 	if (!line_without_string) {
-		return "error in string constants";
+		return ERROR_STRING;
 	}
 	char * line_without_brackets = delete_brackets(line_without_string);
 	free(line_without_string);
 	if (!line_without_brackets) {
-		return "error in brackets";
+		return ERROR_BRACKET;
 	}	
 	free(line_without_brackets);
-	return "that's c-programm without stupid errors";
+	return OK;
 }
